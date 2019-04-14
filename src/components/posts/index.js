ad2c0b3eb5fafe31e7ab8f7e9f3cb982bbdc33c6
haosenhan <haosen.han@empire.ca>
2019-04-14 15:39:43 -0400

    Changes

--- src/components/posts/index.js
+++ src/components/posts/index.js
@@ -1,22 +1,40 @@
 import './posts.css';
-import React from 'react';
-import { PropTypes } from 'prop-types';
+import React from './node_modules/react';
 
-class posts extends React.Component {
+class Posts extends React.Component {
   constructor(props){
       super(props);
       this.state = {
-          articleTitles:['test'],
-          articleExcerpts:['test excerpt']
+          articleInfo:[{
+              title:'test',
+              excerpt:'test excerpt',
+              author:'test author',
+              submittedTime:'2019-01-03'
+          }]
       }
   }
 
+  
+
   render(){
+      const articleTitles = () => {
+          return (
+              <div className="articleExcerpt">
+                  {
+                      this.state.articleInfo.forEach(element => {
+                          <ArticleItem info={element}></ArticleItem>
+                      })
+                  }
+              </div>
+          )
+      }
       return (
-          <div></div>
+          <div className="posts">
+              {articleTitles}
+          </div>
       )
   }
 }
 
 
-export default posts;
+export default Posts;
