b885ea7faea243f4d3e24e7c8fa0a16e79e778fc
haosenhan <haosen.han@empire.ca>
2019-04-27 14:01:52 -0400

    Updating

--- src/components/posts/index.js
+++ src/components/posts/index.js
@@ -1,39 +1,31 @@
-import './posts.css';
-import React from './node_modules/react';
+import './posts.scss';
+import React from 'react';
 
 class Posts extends React.Component {
-  constructor(props){
-      super(props);
-      this.state = {
-          articleInfo:[{
-              title:'test',
-              excerpt:'test excerpt',
-              author:'test author',
-              submittedTime:'2019-01-03'
-          }]
-      }
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            articleInfo: {
+                title: 'test',
+                excerpt: 'test excerpt',
+                author: 'test author',
+                submittedTime: '2019-01-03'
+            }
+        }
+    }
 
-  
 
-  render(){
-      const articleTitles = () => {
-          return (
-              <div className="articleExcerpt">
-                  {
-                      this.state.articleInfo.forEach(element => {
-                          <ArticleItem info={element}></ArticleItem>
-                      })
-                  }
-              </div>
-          )
-      }
-      return (
-          <div className="posts">
-              {articleTitles}
-          </div>
-      )
-  }
+
+    render() {
+        return (
+            <div className="article">
+                <h1>{this.state.articleInfo.title}</h1>
+                <h2>{this.state.articleInfo.author}</h2>
+                <span>{this.state.articleInfo.submittedTime}</span>
+                <p>{this.state.articleInfo.excerpt}</p>
+            </div>
+        )
+    }
 }
 
 
