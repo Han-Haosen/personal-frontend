2659f7e7c69cd0c5488c63040668957cd1d07504
haosenhan <haosen.han@empire.ca>
2019-04-27 14:10:42 -0400

    Adding Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Posts from './components/Posts';
+import Header from './components/Header';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Posts></Posts>         
+         <Header></Header>
+        <Posts></Posts>
       </div>
     );
   }
---
+++ src/components/Header/index.js
@@ -0,0 +1,24 @@
+import './Header.scss';
+import React from 'react';
+
+class Posts extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        }
+    }
+
+
+
+    render() {
+        return (
+            <div class="header">
+                <h2>Personal Blog of Han Haosen</h2>
+            </div>
+        )
+    }
+}
+
+
+export default Posts;
